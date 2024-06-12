397cb832ca131e3b3a924f0a3f5d5e1546129ca5
Abhishek pawarr <abhipawar938@gmail.com>
2024-06-12 14:52:29 +0200

    adding ra_chatbot

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+name= 'cahtbot with rag',
+version= '0.01'
+author-'Abhishek',
+author_email='abhipawar938@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn','openai']
+
+
+)
